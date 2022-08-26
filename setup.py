1cdbab242968996b52cdd902a396ac8d3daaee6c
Marcus McHale <marcus.mchale@nuigalway.ie>
2022-08-26 18:19:30 +0100

    latest build 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='discgrow',
-    version='0.1.1',
+    version='0.1.2',
     packages=['src'],
     scripts=['discgrow.py'],
     url='https://github.com/marcusmchale/discgrow',
