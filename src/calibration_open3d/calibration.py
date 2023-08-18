811b9485e0a808347b7a08fbfc5bda4194e11984
Marcus McHale <marcus.mchale@nuigalway.ie>
2023-08-18 17:11:19 +0100

    migrating to open3d -  at least for the hull definition but maybe the entire gui - many breaking changes

---
+++ src/calibration_open3d/calibration.py
@@ -0,0 +1,20 @@
+"""Interactively define parameters for layout detection and area calculation: circle colour, target hull points"""
+import argparse
+import logging
+from open3d.visualization import gui
+import numpy as np
+
+from .app import AppWindow
+
+logger = logging.getLogger(__name__)
+
+
+def calibrate(args: argparse.Namespace):
+    logger.debug("Initialise the app")
+    gui.Application.instance.initialize()
+    logger.debug("Get window")
+
+    window = AppWindow(1024, 768, args)
+
+    logger.debug("Run")
+    gui.Application.instance.run()
